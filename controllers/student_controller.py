868ac349229f8d3235cf706e827b2d3119d3ae4a
Matija Šeremet <matija.sch03@gmail.com>
2025-11-27 11:53:58 +0100

    [ADD] get student endpoint

--- controllers/student_controller.py
+++ controllers/student_controller.py
@@ -1,20 +1,30 @@
 from fastapi import FastAPI, HTTPException
-
 from dtos.student_dto import StudentCreate
+from model.models import Student
+
+import re
 
 
 students = []
 student_counter = 0
 app = FastAPI()
+email_regex = r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'
 
-@app.post("/students", status_code=201)
+def is_valid_email(email: str) -> bool:
+    return re.match(email_regex, email) is not None
+
+
+@app.post("/students", status_code=201, response_model=Student)
 def create_student(student: StudentCreate):
     global student_counter
 
+    if not is_valid_email(student.email):
+        raise HTTPException(status_code=422, detail="Invalid email format")
+
     # email must be unique
     for s in students:
-        if s["email"] == student.email:
-            raise HTTPException(status_code=400, detail="Email already exists.")
+        if s["email"] == student.email:     # newly created student is dict - look at line 28
+            raise HTTPException(status_code=400, detail="Email already exists")
 
     new_student = {
         "id": str(student_counter),
@@ -26,3 +36,13 @@ def create_student(student: StudentCreate):
 
     students.append(new_student)
     return new_student
+
+
+@app.get("/students/{id}", status_code=200, response_model=Student)
+def get_student(id: str):
+    for student in students:
+        if student["id"] == id:
+            return student
+    raise HTTPException(status_code=404, detail="Student not found")
+
+
